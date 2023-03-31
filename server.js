9d18bc95663bd51c6066040666293a43a728e5d4
adityan2851 <adhi2851@gmail.com>
2023-03-31 11:07:30 +1100

    Initial commit

---
+++ server.js
@@ -0,0 +1,240 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
+  
+  const express = require('express')
+  const app = express()
+  const bcrypt = require('bcrypt')
+  const passport = require('passport')
+  const flash = require('express-flash')
+  const session = require('express-session')
+  const methodOverride = require('method-override')
+  const fs = require('fs');
+  const winston = require('winston');
+  const router = express.Router();
+  const path = require('path');
+  
+  const initializePassport = require('./passport-config')
+  initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+  )
+  
+  const users = []
+  
+  app.set('view-engine', 'ejs')
+  app.use(express.urlencoded({ extended: false }))
+  app.use(flash())
+  app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
+  app.use(methodOverride('_method'))
+  
+  app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.name })
+  })
+  
+  app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
+  })
+  
+  app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  }))
+  
+  app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs')
+  })
+  
+  app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+      })
+      res.redirect('/login')
+    } catch {
+      res.redirect('/register')
+    }
+  })
+  
+  app.delete('/logout', (req, res) => {
+    req.logOut(function(err) {console.log(err)}),
+    res.redirect('/login')
+  })
+  
+  function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+  
+    res.redirect('/login')
+  }
+  
+  function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/')
+    }
+    next()
+  }
+
+  const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'calculate-service' },
+    transports: [
+      //
+      // - Write all logs with importance level of `error` or less to `error.log`
+      // - Write all logs with importance level of `info` or less to `combined.log`
+      //
+      new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'combined.log' }),
+    ],
+  });
+  
+  //
+  // If we're not in production then log to the `console` with the format:
+  // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+  //
+  if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+      format: winston.format.simple(),
+    }));
+  }
+const add= (n1,n2) => {
+    return n1+n2;
+}
+
+const sub=(n1,n2)=>{
+    return n1 - n2
+}
+
+const mul=(n1,n2)=>{
+    return n1 * n2
+}
+
+const div=(n1,n2)=>{
+    return n1 / n2
+}
+
+app.get("/add", (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        logger.error("n1 is incorrectly defined");
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        logger.error("n2 is incorrectly defined");
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    if (n1 === NaN || n2 === NaN) {
+        console.log()
+        throw new Error("Parsing Error");
+    }
+    logger.info('Parameters '+n1+' and '+n2+' received for addition');
+    const result = add(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
+
+app.get("/sub", (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        logger.error("n1 is incorrectly defined");
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        logger.error("n2 is incorrectly defined");
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    if (n1 === NaN || n2 === NaN) {
+        console.log()
+        throw new Error("Parsing Error");
+    }
+    logger.info('Parameters '+n1+' and '+n2+' received for subtraction');
+    const result = sub(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+})
+
+app.get("/mul", (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        logger.error("n1 is incorrectly defined");
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        logger.error("n2 is incorrectly defined");
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    if (n1 === NaN || n2 === NaN) {
+        console.log()
+        throw new Error("Parsing Error");
+    }
+    logger.info('Parameters '+n1+' and '+n2+' received for multiplication');
+    const result = mul(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+})
+
+app.get("/div", (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        logger.error("n1 is incorrectly defined");
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        logger.error("n2 is incorrectly defined");
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    if (n1 === NaN || n2 === NaN) {
+        console.log()
+        throw new Error("Parsing Error");
+    }
+
+    if (n2 === 0){
+        console.log()
+        throw new Error("0 cannot be a denominator")
+    }
+
+    logger.info('Parameters '+n1+' and '+n2+' received for division');
+    const result = div(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+})
+  
+  app.listen(3000)
